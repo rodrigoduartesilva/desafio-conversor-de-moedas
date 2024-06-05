48957f0e283d8311c97dcbccc20be086ae4a0134
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-06-05 18:46:07 -0300

    feat: tratamento de excecao na classe main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import br.com.duarte.conversor.model.Menu;
 import br.com.duarte.conversor.model.Opcao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -9,14 +10,18 @@ public class Main {
         Menu menu = new Menu();
         Opcao opcao = new Opcao();
 
-
         System.out.println(menu.textoMenu());
 
-        do {
-            System.out.print("Selecione uma das opções acima: ");
-            opcao.setOpcaoSelecionada(scanner.nextInt());
+        try {
+            do {
+                System.out.print("Selecione uma das opções acima: ");
+                opcao.setOpcaoSelecionada(scanner.nextInt());
 
-            opcao.valorSelecionado(opcao.getOpcaoSelecionada());
-        } while (opcao.getOpcaoSelecionada() != 7);
+                opcao.valorSelecionado(opcao.getOpcaoSelecionada());
+            } while (opcao.getOpcaoSelecionada() != 7);
+        } catch (InputMismatchException error) {
+            System.err.println("A opção deve ser informada como um número inteiro! ");
+            System.err.println("Erro: " + error);
+        }
     }
 }
